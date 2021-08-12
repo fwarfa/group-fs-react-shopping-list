6926a6e2f614d9b41c71b4b47eb0b2c24e4cff77
Brad Johansen <brad.d.johansen@gmail.com>
2021-08-12 16:45:13 -0500

    end of day, reset needs to be looked at

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,9 +1,15 @@
 import {useState} from 'react';
+import GroupControls from '../GroupControls/GroupControls';
 
-function ShoppingList ({shoppingList}) {
+function ShoppingList ({ deleteAll, resetAll, shoppingList }) {
 
     return (
         <>
+            <h1>Shopping List</h1>
+            <GroupControls 
+                deleteAll = { deleteAll }
+                resetAll = { resetAll }
+            />
             <ul>
                 {shoppingList.map(item => {
                     return (
