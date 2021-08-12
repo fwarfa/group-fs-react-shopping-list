f10ab30c03ce11ddfcebe6c8c68d43cc386848e0
Farah Warfa <fwarfa@gmail.com>
2021-08-12 11:56:42 -0500

    created post axios, handshake with additem

--- database.sql
+++ database.sql
@@ -4,9 +4,12 @@ CREATE TABLE "shopping-list"(
 	"id" serial PRIMARY KEY,
 	"name" varchar(80),
 	"quantity" integer, 
-	"unit" varchar (20)
+	"unit" varchar (20),
+	"bought" boolean default false
 	);
 DROP TABLE "shopping-list";
 	
 INSERT INTO "shopping-list"(name, quantity, unit)
 VALUES ('Example', 10, '5 cups');
+
+Select * from "shopping-list";
